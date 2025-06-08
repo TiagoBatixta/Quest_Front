4b94e88f2b6065367f3f533987d49203eb96165a
Diego <odiegodev@gmail.com>
2025-06-08 16:34:03 -0300

    feat: adiciona rota de cadastro e recuperação de senha

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,18 @@
-import Home from "./pages/Home/Home"
+import Home from './pages/Home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from "./pages/Login/Login";
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Recover from './pages/Recover/Recover';
 
 export default function App() {
   return (
-    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recover" element={<Recover />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
