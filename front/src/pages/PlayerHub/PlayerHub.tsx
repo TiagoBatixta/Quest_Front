47825a4d6794c2cb16e911f843609bac06e0f1c0
Diego <odiegodev@gmail.com>
2025-06-12 20:59:17 -0300

    refactor: refatora o playerHub em componentes menores

--- front/src/pages/PlayerHub/PlayerHub.tsx
+++ front/src/pages/PlayerHub/PlayerHub.tsx
@@ -1,6 +1,8 @@
 import styles from './PlayerHub.module.scss';
 import logo from './../../assets/QUEST.png';
 import { Link } from 'react-router-dom';
+import FormSession from '../../componentes/FormSession/FormSession';
+import JoinSessionDashboard from '../../componentes/JoinSessionDashboard/JoinSessionDashboard';
 
 import image1 from '../../assets/avatar/avatar1.png';
 import image2 from '../../assets/avatar/avatar2.png';
@@ -8,8 +10,6 @@ import image3 from '../../assets/avatar/avatar3.png';
 import image4 from '../../assets/avatar/avatar4.png';
 import image5 from '../../assets/avatar/avatar5.png';
 import image6 from '../../assets/avatar/avatar6.png';
-import { UsersRound } from 'lucide-react';
-
 const images = [image1, image2, image3, image4, image5, image6];
 
 const jogador = {
@@ -49,77 +49,8 @@ export default function PlayerHub() {
           </Link>
         </div>
 
-        <form>
-          <label htmlFor="session">
-            <input
-              type="text"
-              name="session"
-              id="session"
-              placeholder="ID da sessão"
-            />
-          </label>
-          <button className={'secondary-btn'} type="submit">
-            Entrar na partida
-          </button>
-        </form>
-
-        <section className={styles.dashboard}>
-          <header>
-            <UsersRound size={48} />
-            <h2>Partidas</h2>
-          </header>
-
-          <ul>
-            <li>
-              <img
-                src={images[1]}
-                alt="Avatar do usuário"
-                width={80}
-                height={80}
-              />
-              <p>Niwan</p>
-              <button disabled className={`${styles.disabled} secondary-btn`}>
-                jogar junto
-              </button>
-            </li>
-            <li>
-              <img
-                src={images[4]}
-                alt="Avatar do usuário"
-                width={80}
-                height={80}
-              />
-              <p>Joãozinho da 12</p>
-              <button className={'secondary-btn'}>jogar junto</button>
-            </li>
-            <li>
-              <img
-                src={images[5]}
-                alt="Avatar do usuário"
-                width={80}
-                height={80}
-              />
-              <p>Pedrinho Matador</p>
-              <button className={'secondary-btn'}>jogar junto</button>
-            </li>
-            <li>
-              <img
-                src={images[3]}
-                alt="Avatar do usuário"
-                width={80}
-                height={80}
-              />
-              <p>meMataDeUmaVez</p>
-              <button className={'secondary-btn'}>jogar junto</button>
-            </li>
-          </ul>
-
-          <footer>
-            <button className={'btn'} type="submit">
-              Criar Sala
-            </button>
-          </footer>
-        </section>
+        <FormSession />
+        <JoinSessionDashboard />
       </section>
     </div>
   );
